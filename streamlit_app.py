0c26b3cb150bbc2c59d8897e03c9656f16d90c79
Prashanthpcr <70429661+Prashanthpcr@users.noreply.github.com>
2024-05-12 20:18:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,112 +1,49 @@
-# import the rquired libraries.
 import numpy as np
+from PIL import Image, ImageOps
 import cv2
-from keras.models import load_model
 import streamlit as st
-from tensorflow import keras
-from keras.preprocessing.image import img_to_array
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array, array_to_img
 
-# Define the emotions.
-emotion_labels = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Sadness','Surprise', 'Neutral']
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-# Load model.
-classifier =load_model('model1.h5')
+model = load_model('modelk1.keras')
+model.load_weights('modelk1.weights.keras')
 
-# load weights into new model
-classifier.load_weights("model_weights1.weights.h5")
-
-# Load face using OpenCV
-try:
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-except Exception:
-    st.write("Error loading cascade classifiers")
-
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        #image gray
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(
-            image=img_gray, scaleFactor=1.3, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img=img, pt1=(x, y), pt2=(
-                x + w, y + h), color=(0, 255, 255), thickness=2)
-            roi_gray = img_gray[y:y + h, x:x + w]
-            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-            if np.sum([roi_gray]) != 0:
-                roi = roi_gray.astype('float') / 255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi, axis=0)
-                prediction = classifier.predict(roi)[0]
-                maxindex = int(np.argmax(prediction))
-                finalout = emotion_labels[maxindex]
-                output = str(finalout)
-            label_position = (x, y-10)
-            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        return img
-
-def main():
-    # Face Analysis Application #
-    st.title("Real Time Face Emotion Detection Application 😠🤮😨😀😐😔😮")
-    activiteis = ["Home", "Live Face Emotion Detection", "About"]
-    choice = st.sidebar.selectbox("Select Activity", activiteis)
-    st.sidebar.markdown(
-        """ Developed by Anish Johnson
-            [LinkedIn](https://www.linkedin.com/in/anish-johnson-594110208/)""")
-
-    # Homepage.
-    if choice == "Home":
-        html_temp_home1 = """<div style="background-color:#FC4C02;padding:0.5px">
-                             <h4 style="color:white;text-align:center;">
-                            Start Your Real Time Face Emotion Detection.
-                             </h4>
-                             </div>
-                             </br>"""
-
-        st.markdown(html_temp_home1, unsafe_allow_html=True)
-        st.write("""
-        * An average human spends about 10 to 15hrs a day staring at a computer screen, during which our facial expressions keep on changing. 
-        * Sometimes we laugh, sometimes we cry, sometimes we get angry, and sometimes get scared by our face when the camera turns on accidentally.
-        * But ever wondered; whether the computer that we give all this attention to is even capable of recognizing these emotions?
-        
-        Let's find out...
-        1. Click the dropdown list in the top left corner and select Live Face Emotion Detection.
-        2. This takes you to a page which will tell if it recognizes your emotions.
-                 """)
-
-    # Live Face Emotion Detection.
-    elif choice == "Live Face Emotion Detection":
-        st.header("Webcam Live Feed")
-        st.subheader('''
-        Welcome to the other side of the SCREEN!!!
-        * Get ready with all the emotions you can express. 
-        ''')
-        st.write("1. Click Start to open your camera and give permission for prediction")
-        st.write("2. This will predict your emotion.")
-        st.write("3. When you done, click stop to end.")
-        webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
-
-    # About.
-    elif choice == "About":
-        st.subheader("About this app")
-        html_temp_about1= """<div style="background-color:#36454F;padding:30px">
-                                    <h4 style="color:white;">
-                                     This app predicts facial emotion using a Convolutional neural network.
-                                     Which is built using Keras and Tensorflow libraries.
-                                     Face detection is achived through openCV.
-                                    </h4>
-                                    </div>
-                                    </br>
-                                    """
-        st.markdown(html_temp_about1, unsafe_allow_html=True)
+st.write("""# Face Expression Recognition""")
+st.write("This Model predicts the expression on a persons face.")
+file = st.file_uploader("Please upload an image file", type=["jpg"])
 
 
-    else:
-        pass
+def import_and_predict(image_data, model):
+    size = (150, 150)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = np.asarray(image)
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    face_haar_cascade = cv2.CascadeClassifier(
+        'haarcascade_frontalface_default.xml')
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+    for (x, y, w, h) in faces_detected:
+        roi_gray = gray_img[y:y+w, x:x+h]
+        roi_gray = cv2.resize(roi_gray, (48, 48))
+        img_pixels = img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+        img_pixels /= 255
+        predictions = model.predict(img_pixels)
+        max_index = np.argmax(predictions[0])
+        emotions = ('ANGRY 😡', 'HAPPY 😀', 'NEUTRAL 😐', 'SAD 🙁')
+        predicted = emotions[max_index]
+    return predicted
 
 
-if __name__ == "__main__":
-    main()
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    st.text("The given image has the following expression:")
+    st.write(prediction)
+    st.text("*****Built By Data Science Community SRM with 💜 *****")
